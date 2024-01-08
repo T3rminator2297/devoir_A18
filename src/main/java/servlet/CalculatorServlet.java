65fd8d83556b7341676d73b54b374581b0c935be
T3rminator2297 <francis2297@hotmail.com>
2024-01-08 16:41:43 -0500

    Création du servlet CalculatorServlet.java

---
+++ src/main/java/servlet/CalculatorServlet.java
@@ -0,0 +1,8 @@
+package servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/calculator")
+public class CalculatorServlet  extends HttpServlet {
+}
